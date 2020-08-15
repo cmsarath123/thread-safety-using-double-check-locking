2d0d6db76de5e2b4c7d25984d193e7d6b7588d6b
sarathkumarc <cmsarath123@gmail.com>
2020-08-15 20:41:58 +0530

    singleton demo

---
+++ singletondemo/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace singletondemo
+{
+    public class Singleton
+    {
+        private static int ObjectsCount = 0;
+        public static Singleton instance = null;
+        private static readonly object padlock = new object();
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                            instance = new Singleton();
+                    }
+                }
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            ObjectsCount++;
+            Console.WriteLine($"instances {ObjectsCount}");
+        }
+
+        public void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
